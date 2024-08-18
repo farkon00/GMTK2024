b5623b6c846334d9e6fc6ab6fc7deeb31431de72
rs-pro0 <rostyslav.yabchenko@gmail.com>
2024-08-18 17:30:12 +0300

    Now you can't change size of multiple planets at once

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,11 +3,34 @@ extends Node
 
 static var instance: GameManager
 var is_gameover: bool = false
+var currently_changing: Planet = null
 
 func _init():
-	instance = self
+    instance = self
 
 func gameover():
-	is_gameover = true
-	$"GameOverUI".set_process(true)
-	$"GameOverUI".visible = true
+    is_gameover = true
+    $"GameOverUI".set_process(true)
+    $"GameOverUI".visible = true
+
+func _input(event):
+    if !(event is InputEventMouseButton) :
+        return
+    if (event.button_index != MOUSE_BUTTON_LEFT && event.button_index != MOUSE_BUTTON_RIGHT):
+        return
+    if !event.pressed:
+        if currently_changing != null:
+            currently_changing.laser_state = 0
+        currently_changing = null
+        return
+    if currently_changing != null:
+        return
+    var planets = $"../Planets".get_children() as Array[Planet]
+    for planet in planets:
+        if planet.get_node("CollisionShape2D").shape.get_rect().has_point(planet.get_local_mouse_position()):
+            currently_changing = planet
+            if event.button_index == MOUSE_BUTTON_LEFT:
+                planet.laser_state = 1
+            else:
+                planet.laser_state = -1
+            return
--- Scripts/Planet.gd
+++ Scripts/Planet.gd
@@ -10,20 +10,12 @@ static var G = 4000
 var laser_state: int = 0
 
 func get_force_vector(pos: Vector2):
-	return G * mass / (position - pos).length_squared() * (position - pos).normalized()
+    return G * mass / (position - pos).length_squared() * (position - pos).normalized()
 
 func _process(delta):
-	if GameManager.instance.is_gameover: return
-	if $"CollisionShape2D".shape.get_rect().has_point(get_local_mouse_position()):
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			laser_state = 1
-		elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-			laser_state = -1
-	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && laser_state == 1:
-		laser_state = 0
-	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) && laser_state == -1:
-		laser_state = 0
-	mass += laser_speed * delta * laser_state
-	mass = min(max(mass, mass_min), mass_max)
-	
-	scale = Vector2(1, 1) * sqrt(mass / density)
+    if GameManager.instance.is_gameover: return
+    mass += laser_speed * delta * laser_state
+    mass = min(max(mass, mass_min), mass_max)
+
+    scale = Vector2(1, 1) * sqrt(mass / density)
+

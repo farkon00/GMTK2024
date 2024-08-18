0806b49f1e494e6d08faa26f5fa1c50ba250a408
farkon00 <59134591+farkon00@users.noreply.github.com>
2024-08-19 00:04:38 +0300

    Fixed a teleportation bug in portals
    When a missile teleports, there was a chance for it to instantly explode,
    because of incorrectly handling the Portal.recently_teleported array.

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -10,8 +10,8 @@ func _on_area_2d_body_entered(body: Node2D):
 
 func _on_teleportation_area_body_entered(body: Node2D):
 	if !(body is Missile): return
-	if connects_to.recently_teleported.has(body):
-		connects_to.recently_teleported.erase(body)
-		return
+	if connects_to.recently_teleported.has(body): return
 	body.position = connects_to.position + Vector2(0, body.position.y - position.y)
 	recently_teleported.append(body)
+	await get_tree().create_timer(0.3).timeout
+	recently_teleported.erase(body)

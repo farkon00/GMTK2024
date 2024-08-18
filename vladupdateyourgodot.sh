57d5e6fb7966c9e0d900d47c4b90149b02c28e97
rs-pro0 <rostyslav.yabchenko@gmail.com>
2024-08-18 15:16:39 +0300

    vlad update your godot

---
+++ vladupdateyourgodot.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+wget https://github.com/godotengine/godot/releases/download/4.3-stable/Godot_v4.3-stable_linux.x86_64.zip
+unzip Godot_v4.3-stable_linux.x86_64.zip
+./Godot_v4.3-stable_linux.x86_64

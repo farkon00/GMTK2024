f91cc1b031475e333b73815acee5afdf23cbc9e5
farkon00 <59134591+farkon00@users.noreply.github.com>
2024-08-18 23:48:24 +0300

    Add portals

---
+++ Scripts/Portal.gd
@@ -0,0 +1,17 @@
+class_name Portal
+extends SpaceBody
+
+@export var connects_to: Portal
+var recently_teleported: Array[Missile] = []
+
+func _on_area_2d_body_entered(body: Node2D):
+	if connects_to.recently_teleported.has(body): return
+	super(body)
+
+func _on_teleportation_area_body_entered(body: Node2D):
+	if !(body is Missile): return
+	if connects_to.recently_teleported.has(body):
+		connects_to.recently_teleported.erase(body)
+		return
+	body.position = connects_to.position + Vector2(0, body.position.y - position.y)
+	recently_teleported.append(body)
